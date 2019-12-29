0eb3ff4cc2a058019638f5eadd116c39a5562732
fengqingyang <1454626672@qq.com>
2019-12-29 16:10:27 +0800

    更新80

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -17,14 +17,14 @@ public class OrderController
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping("/consumer/payment/create") //客户端用浏览器是get请求，但是底层实质发送post调用服务端8001
-    public CommonResult create(Payment payment)
+    @GetMapping("/consumer/payment/createPayment") //客户端用浏览器是get请求，但是底层实质发送post调用服务端8001
+    public CommonResult createPayment(Payment payment)
     {
         return restTemplate.postForObject(PaymentSrv_URL + "/payment/create",payment,CommonResult.class);
     }
 
 
-    @GetMapping("/consumer/payment/get/{id}")
+    @GetMapping("/consumer/payment/getPayment/{id}")
     public CommonResult getPayment(@PathVariable Long id)
     {
         return restTemplate.getForObject(PaymentSrv_URL + "/payment/get/"+id, CommonResult.class, id);
